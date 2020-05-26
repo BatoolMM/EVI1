c63dba9ac6d9d5ebec35875ac6fd8fcbec154f29
Batool Almarzouq <batool@liverpool.ac.uk>
2020-05-26 09:13:17 +0100

    added the analysis of the MD

---
+++ unpho_complex/last_20ns_new/script.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+#SBATCH --export=ALL
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=40
+#SBATCH --mail-user=batool@liverpool.ac.uk
+#SBATCH --mail-type=ALL
+#SBATCH -t 3-00:00:00
+
+export OMP_NUM_THREADS=$SLURM_NTASKS
+module load apps/anaconda3/5.2.0
+module load apps/gromacs/5.1.4/gcc-5.5.0+openmpi-1.10.7+fftw3_float-3.3.4+fftw3_double-3.3.4
+
+echo 0 | gmx trjconv -f md_fit.xtc -s md_400ns.tpr -b 380000 -e 400000 -o md_fit1.xtc
+echo 19 18| ./g_mmpbsa -f md_fit1.xtc -s md_400ns.tpr -n index.ndx -pdie 2 -decomp
+echo 19 18| ./g_mmpbsa -f md_fit1.xtc -s md_400ns.tpr -n index.ndx -i apolar_sasa.mdp -nomme -pbsa -decomp -apol sasa.xvg -apcon sasa_contrib.dat
+echo 19 18| ./g_mmpbsa -f md_fit1.xtc -s md_400ns.tpr -n index.ndx -i apolar_sav.mdp -nomme -pbsa -decomp -apol sav.xvg -apcon sav_contrib.dat
+echo 19 18| ./g_mmpbsa -f md_fit1.xtc -s md_400ns.tpr -n index.ndx -i polar.mdp -nomme -pbsa -decomp
+
+python3 MmPbSaStat.py -m energy_MM.xvg -p polar.xvg -a sasa.xvg
+
+
+
